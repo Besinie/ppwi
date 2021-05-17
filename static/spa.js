173964bf4450f35e0edcbd5086a261fdebeddb77
Daniel Szot <64105813+Besinie@users.noreply.github.com>
2021-05-17 19:45:09 +0200

    Add files via upload

---
+++ static/spa.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+        $(".nav1").click(function(){
+          document.title = "Daniel Szot - PwI - Main Page";
+          $(".container").hide();
+          $(".algorithm").hide();
+          $(".mainh2").show();
+          $(".asideh2").show();
+        });
+      });
+      
+
+    $(document).ready(function(){
+        $(".nav2").click(function(){
+          document.title = "Daniel Szot - PwI - Main Page";
+          $(".container").hide();
+          $(".algorithm").hide();
+          $(".mainh2").show();
+          $(".asideh2").show();
+        });
+      });      
+
+    $(document).ready(function(){
+        $(".nav3").click(function(){
+          document.title = "Daniel Szot - PwI - Tic Cube Toe";
+          $(".container").show();
+          $(".algorithm").hide();
+          $(".mainh2").hide();
+          $(".asideh2").hide();
+        });
+      });
+
+    $(document).ready(function(){
+        $(".nav4").click(function(){
+          document.title = "Daniel Szot - PwI - Speedcubing";
+          $(".container").hide();
+          $(".algorithm").hide();
+          $(".mainh2").hide();
+          $(".asideh2").hide();
+        });
+      });
+
+      $(document).ready(function(){
+        $(".nav5").click(function(){
+          document.title = "Daniel Szot - PwI - Algorytmy";
+          $(".container").hide();
+          $(".algorithm").show();
+          $(".mainh2").hide();
+          $(".asideh2").hide();
+        });
+      });
+
+      $(document).ready(function(){
+        $(".nav6").click(function(){
+          document.title = "Daniel Szot - PwI - Jak zacząć";
+          $(".container").hide();
+          $(".algorithm").hide();
+          $(".mainh2").hide();
+          $(".asideh2").hide();
+        });
+      });
+
